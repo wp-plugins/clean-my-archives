4797d13cadc1e294da218ddd46c75220b00c306d
greenshady <greenshady@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-19 18:20:12 +0000

    Updating version 1.0.0 of the Cleaner Gallery plugin.
    git-svn-id: https://plugins.svn.wordpress.org/clean-my-archives/tags/1.0.0@1225462 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ clean-my-archives.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Plugin Name: Clean My Archives
+ * Plugin URI:  http://themehybrid.com/plugins/clean-my-archives
+ * Description: A plugin that displays a full archive of posts by month and year with the <code>[clean-my-archives]</code> shortcode.
+ * Version:     1.0.0
+ * Author:      Justin Tadlock
+ * Author URI:  http://justintadlock.com
+ *
+ * Clean My Archives is a plugin developed to simplify the process of adding a list of archives to your
+ * site.  So many archives plugins make things overly complex or add a lot of junk to the page like unneeded
+ * JavaScript.  This plugin was created to clean your archives page.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @package   CleanMyArchives
+ * @version   1.0.0
+ * @author    Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2008 - 2015, Justin Tadlock
+ * @link      http://themehybrid.com/plugins/clean-my-archives
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+# Set up the plugin.
+add_action( 'plugins_loaded', 'clean_my_archives_setup' );
+
+/**
+ * Sets up the plugin and calls its default actions.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function clean_my_archives_setup() {
+
+	// Load translations.
+	load_plugin_textdomain( 'clean-my-archives', false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ) . 'languages' );
+
+	// Register shortcodes.
+	add_action( 'init', 'clean_my_archives_shortcodes' );
+
+	// Delete the cache when a post is saved.
+	add_action( 'save_post', 'clean_my_archives_delete_cache' );
+}
+
+/**
+ * Registers shortcodes for the plugin.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function clean_my_archives_shortcodes() {
+
+	// Add [clean-my-archives] shortcode.
+	add_shortcode( 'clean-my-archives', 'clean_my_archives' );
+}
+
+/**
+ * Returns a formated archive of all posts for the blog.
+ *
+ * @since  0.1.0
+ * @access public
+ * @param  array   $attr   The shortcode attributes.
+ * @return string  $clean  Formatted archives.
+ */
+function clean_my_archives( $attr = array() ) {
+
+	// Set up some default variables that need to be empty.
+	$clean         = '';
+	$current_year  = '';
+	$current_month = '';
+	$current_day   = '';
+	$cache         = array();
+
+	// Default arguments.
+	$defaults = array(
+		'limit'     => -1,
+		'year'      => '',
+		'month'     => '',
+		'post_type' => 'post'
+	);
+
+	$attr = shortcode_atts( $defaults, $attr, 'clean-my-archives' );
+
+	// Set up some arguments to pass to WP_Query.
+	$args = array(
+		'posts_per_page'      => intval( $attr['limit'] ),
+		'year'                => $attr['year'] ? absint( $attr['year'] ) : '',
+		'monthnum'            => $attr['month'] ? absint( $attr['month'] ) : '',
+		'post_type'           => is_array( $attr['post_type'] ) ? $attr['post_type'] : explode( ',', $attr['post_type'] ),
+		'ignore_sticky_posts' => true,
+	);
+
+	// Create a unique key for this particular set of archives.
+	$key = md5( serialize( array_values( $args ) ) );
+
+	// Check for a cached archives.
+	$cache = wp_cache_get( 'clean_my_archives' );
+
+	// If there is a cached archive, return it instead of doing all the work we've already done.
+	if ( is_array( $cache ) && ! empty( $cache[ $key ] ) )
+		return $cache[ $key ];
+
+	// Query posts from the database.
+	$loop = new WP_Query( $args );
+
+	// If posts were found, format them for output.
+	if ( $loop->have_posts() ) {
+
+		// Loop through the individual posts.
+		while ( $loop->have_posts() ) {
+
+			// Set up the post.
+			$loop->the_post();
+
+			// Get the post's year and month. We need this to compare it with the previous post date.
+			$year   = get_the_time( 'Y' );
+			$month  = get_the_time( 'm' );
+			$daynum = get_the_time( 'd' );
+
+			// If the current date doesn't match this post's date, we need extra formatting.
+			if ( $current_year !== $year || $current_month !== $month ) {
+
+				// Close the list if this isn't the first post.
+				if ( $current_month && $current_year )
+					$clean .= '</ul>';
+
+				// Set the current year and month to this post's year and month.
+				$current_year  = $year;
+				$current_month = $month;
+				$current_day   = '';
+
+				// Add a heading with the month and year and link it to the monthly archive.
+				$clean .= sprintf(
+					'<h2 class="month-year"><a href="%s">%s</a></h2>',
+					esc_url( get_month_link( $current_year, $current_month ) ),
+					esc_html( get_the_time( __( 'F Y', 'clean-my-archives' ) ) )
+				);
+
+				// Open a new unordered list.
+				$clean .= '<ul>';
+			}
+
+			// Get the post's day.
+			$day = sprintf( '<span class="day">%s</span>', get_the_time( esc_html__( 'd:', 'clean-my-archives' ) ) );
+
+			// Translators: %d is the comment count.
+			$comments_num = sprintf( esc_html__( '(%d)', 'clean-my-archives' ), get_comments_number() );
+			$comments     = sprintf( '<span class="comments-number">%s</span>',  $comments_num );
+
+			// Check if there's a duplicate day so we can add a class.
+			$duplicate_day = $current_day && $daynum === $current_day ? ' class="day-duplicate"' : '';
+			$current_day   = $daynum;
+
+			// Add the post list item to the formatted archives.
+			$clean .= the_title(
+				sprintf( '<li%s>%s <a href="%s" rel="bookmark">', $duplicate_day, $day, esc_url( get_permalink() ) ),
+				sprintf( '</a> %s</li>', $comments ),
+				false
+			);
+		}
+
+		// Close the final unordered list.
+		$clean .= '</ul>';
+	}
+
+	// Wrap the list in a `<div>`.
+	if ( $clean )
+		$clean = sprintf( '<div class="clean-my-archives">%s</div>', $clean );
+
+	// Reset the query to the page's original query.
+	wp_reset_postdata();
+
+	// Make sure $cache is an array.
+	if ( ! is_array( $cache ) )
+		$cache = array();
+
+	// Set the cache for the plugin, so caching plugins can make this super fast.
+	$cache[ $key ] = $clean;
+	wp_cache_set( 'clean_my_archives', $cache );
+
+	// Return the formatted archives.
+	return $clean;
+}
+
+/**
+ * Deletes the archive cache for users that are using a persistent-caching plugin.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function clean_my_archives_delete_cache() {
+	wp_cache_delete( 'clean_my_archives' );
+}
---
+++ readme.txt
@@ -0,0 +1,94 @@
+=== Clean My Archives ===
+
+Contributors: greenshady
+Donate link: http://themehybrid.com/donate
+Tags: archives, shortcode
+Requires at least: 3.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+An easy-to-use shortcode for displaying post archives on your site.
+
+== Description ==
+
+The Clean My Archives plugin was developed because so many archives plugins were overly complicated.  I wanted something extremely simple to use that simply got the job done on my own [site's archives](http://justintadlock.com/archives).
+
+Therefore, I created a simple `[clean-my-archives]` shortcode that you can place on any page (or any shortcode-ready area) and list your post archives by month and year.
+
+This plugin also integrates with WordPress caching plugins.  So, long lists of archives can be cached for later use and loaded quickly.  If you have many years of blog posts, I highly recommend some sort of persistent caching or paginating your archives (see FAQ).
+
+### Professional Support
+
+If you need professional plugin support from me, the plugin author, you can access the support forums at [Theme Hybrid](http://themehybrid.com/support), which is a professional WordPress help/support site where I handle support for all my plugins and themes for a community of 60,000+ users (and growing).
+
+### Plugin Development
+
+If you're a theme author, plugin author, or just a code hobbyist, you can follow the development of this plugin on it's [GitHub repository](https://github.com/justintadlock/clean-my-archives). 
+
+### Donations
+
+Yes, I do accept donations.  If you want to buy me a beer or whatever, you can do so from my [donations page](http://themehybrid.com/donate).  I appreciate all donations, no matter the size.  Further development of this plugin is not contingent on donations, but they are always a nice incentive.
+
+== Installation ==
+
+1. Uzip the `clean-my-archives.zip` folder.
+2. Upload the `clean-my-archives` folder to your `/wp-content/plugins` directory.
+3. In your WordPress dashboard, head over to the *Plugins* section.
+4. Activate *Clean My Archives*.
+
+== Frequently Asked Questions ==
+
+### Why was this plugin created?
+
+First and foremost, I wanted a simple archives solution for, what's now, over 10 years of blog posts.  You can check out [my archives](http://justintadlock.com/archives) to see how the plugin performs.
+
+The second reason was to share my solution with other users who want the same simplicity.
+
+### How do I use it?
+
+Add the `[clean-my-archives]` shortcode to a shortcode-ready area, such as the page editor. That's it.  You have nothing more to do.
+
+Of course, there are some other configuration options. Examples follow.
+
+#### Limit the number of posts with the `limit` parameter:
+
+	[clean-my-archives limit="100"]
+
+#### Use the `year` parameter to limit to a year:
+
+	[clean-my-archives year="2013"]
+
+#### Use the `month` parameter to limit by month:
+
+	[clean-my-archives month="12"]
+
+#### Load specific post types with the `post_type` parameter
+
+	[clean-my-archives post_type="post"]
+
+	[clean-my-archives post_type="post, page"]
+
+### Does it support pagination?
+
+Sort of.  Technically, it doesn't.  However, WordPress pages support pagination.  So, you could enter the following in your page editor to paginate by year.
+
+	[clean-my-archives year="2013"]
+
+	<!-- nextpage -->
+
+	[clean-my-archives year="2012"]
+
+	<!-- nextpage -->
+
+	[clean-my-archives year="2011"]
+
+== Screenshots ==
+
+You can see a [live demo here](http://justintadlock.com/archives).
+
+1. Screenshot of the plugin in action my archives page.
+
+== Changelog ==
+
+To view the change log, look into the `changelog.md` file included with the plugin or visit the [online change log](https://github.com/justintadlock/clean-my-archives/blob/master/changelog.md).
